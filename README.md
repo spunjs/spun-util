8b2c16e0df176fb328bdab13e7a23d4ac00bd875
jsdevel <js.developer.undefined@gmail.com>
2014-06-30 23:24:38 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,49 @@
+# spun-util [![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Coveralls Status][coveralls-image]][coveralls-url]
+> Utility functions for spun compilers.
+
+## Errors
+
+See `lib/errors`
+
+## Regex
+
+See `lib/regex`
+
+## CLI
+
+See `lib/cli`
+
+##LICENSE
+``````
+The MIT License (MIT)
+
+Copyright (c) 2014 Joseph Spencer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+``````
+
+[downloads-image]: http://img.shields.io/npm/dm/spun-util.svg
+[npm-url]: https://npmjs.org/package/spun-util
+[npm-image]: http://img.shields.io/npm/v/spun-util.svg
+
+[travis-url]: https://travis-ci.org/jsdevel/spun-util
+[travis-image]: http://img.shields.io/travis/jsdevel/spun-util.svg
+
+[coveralls-url]: https://coveralls.io/r/jsdevel/spun-util
+[coveralls-image]: http://img.shields.io/coveralls/jsdevel/spun-util/master.svg
---
+++ test/spun.tests.js
@@ -0,0 +1,111 @@
+'use strict';
+
+var should = require('should');
+
+describe('spun-util', function(){
+  var sinon = require('sinon');
+  var processTitle = 'node';
+  var _process = {
+    /* jshint ignore:start */
+    set title(title){
+      processTitle = title;
+    },
+    /* jshint ignore:end */
+    exit: sinon.stub()
+  };
+  var prequire = require('proxyquire').noCallThru();
+  var cli = prequire('../lib/cli', {
+    './process': _process
+  });
+  var sUtil = prequire('..', {
+    './cli': cli
+  });
+
+
+  before(function(){
+    _process.exit.reset();
+  });
+
+  it('should change process title to spun', function(){
+    processTitle.should.equal('spun');
+  });
+
+  describe('regex', function(){
+    var regex = sUtil.regex;
+
+    describe('.string', function(){
+      it('should match appropriately', function(){
+        regex.string.exec('"asdf"')[1].should.equal('asdf');
+        regex.string.test('\'asdf\'').should.be.false;
+        regex.string.test('asdf').should.be.false;
+      });
+    });
+
+    describe('.assignment', function(){
+      it('should match appropriately', function(){
+        regex.assignment.exec('f="asdf"')[1].should.equal('f');
+        regex.assignment.exec('f="asdf"')[2].should.equal('asdf');
+        regex.assignment.test('f=\'asdf\'').should.be.false;
+        regex.assignment.test('f=asdf').should.be.false;
+      });
+    });
+
+    describe('.stringQuotes', function(){
+      it('should match appropriately', function(){
+        regex.stringQuotes.test('f"').should.be.true;
+        regex.stringQuotes.test('"f').should.be.false;
+      });
+    });
+  });
+
+  describe('cli', function(){
+    describe('log', function(){
+      it('should log to console', function(){
+        sUtil.cli.log('log');
+      });
+    });
+
+    describe('warn', function(){
+      it('should warn to console', function(){
+        sUtil.cli.warn('warning');
+      });
+    });
+
+    describe('error', function(){
+      it('should error to console', function(){
+        sUtil.cli.error('error');
+      });
+    });
+
+    describe('help', function(){
+      it('should call process.exit(0) with no args', function(){
+        sUtil.cli.help();
+        sinon.assert.calledWith(_process.exit, 0);
+      });
+
+      it('should call process.exit() with an exit code', function(){
+        sUtil.cli.help(5);
+        sinon.assert.calledWith(_process.exit, 5);
+      });
+    });
+  });
+
+  describe('errors', function(){
+    var errors = sUtil.errors;
+
+    Object.keys(errors.compiling).forEach(function(error){
+      describe('.' + error, function(){
+        it('should have error properties when thrown ' + error, function(){
+          var line = {number: 5, text: 'asdf'};
+          try {
+            throw new errors.compiling[error](line, error);
+          } catch(e) {
+            e.message.should.be.type('string');
+            e.stack.should.be.an.instanceOf(Object);
+            e.should.be.an.instanceOf(errors.compiling[error]);
+          }
+        });
+      });
+    });
+  });
+});
