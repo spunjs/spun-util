3edfe61131367ce89887348859a74136ea5f3e3e
jsdevel <js.developer.undefined@gmail.com>
2014-07-10 13:20:06 -0700

    Removing setup errors.

--- test/errors.tests.js
+++ test/errors.tests.js
@@ -1,9 +1,26 @@
 'use strict';
 
+var f = require('util').format;
+
 describe('errors', function(){
   var errors = require('../lib/errors');
 
-  describe('.createLineErrors', function(){
+  [
+    'CompileError',
+    'ParseError',
+    'SpunError',
+    'ValidationError'
+  ].forEach(function(constructor){
+    it(f('should expose a %s constructor and be an instance of SpunError', constructor), function(){
+      var instance;
+      errors[constructor].should.be.type('function');
+      instance = new errors[constructor]();
+      instance.should.be.an.instanceOf(errors[constructor]);
+      instance.should.be.an.instanceOf(errors.SpunError);
+    });
+  });
+
+  describe('.createLineErrors()', function(){
     var createLineErrors = errors.createLineErrors;
 
     it('should assign errors to the target', function(){
@@ -13,6 +30,18 @@ describe('errors', function(){
       var lines = [line];
       createLineErrors(target, Parent, [{name: 'FooError', message: 'wow'}]);
       (new target.FooError(line, lines)).should.be.an.instanceOf(Parent);
+    });
+  });
+
+  describe('.getStack()', function(){
+    var getStack = errors.getStack;
+
+    it('should return line info', function(){
+      var line1 = {file: 'line1file', number: 1};
+      var line2 = {file: 'line2file', number: 2};
+      var lines = [line1, line2];
+      var stack = getStack(line2, lines);
+      stack.join('').should.equal('line2file (line 2)line1file (line 1)');
     });
   });
 });
